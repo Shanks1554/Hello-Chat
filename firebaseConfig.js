f9b4be31965fdff2657c5a3e8cc45a906c5a8048
Shanks1554 <146436612+Shanks1554@users.noreply.github.com>
2024-08-14 18:59:41 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import 'firebase/compat/storage';
 import 'firebase/compat/firestore'; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoWGzid2rtcYtVjl7YR_pGPyPE-e1GVsE",
-  authDomain: "fir-chatapp-6d6ec.firebaseapp.com",
-  projectId: "fir-chatapp-6d6ec",
-  storageBucket: "fir-chatapp-6d6ec.appspot.com",
-  messagingSenderId: "911567679708",
-  appId: "1:911567679708:web:f7f1dcd84ee577ad5a4419"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 if (!firebase.apps.length) {
